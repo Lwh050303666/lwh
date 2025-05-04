0001606b65b38fca999d5a7822501c3f07a724b5
李维皓 <lwh2212789853@gmail.com>
2025-05-04 23:45:46 +0800

    哈希(hash)结构

---
+++ 2025-5-4/2025-5-4/FileName.cpp
@@ -0,0 +1,78 @@
+//#include <stdio.h>
+//const int N = 1e4;
+//int L, R, arr[N], count;
+//int main() {
+//    scanf_s("%d %d", &L, &R);
+//    for (int i = L; L <= R; i++)
+//    {
+//        while (i) {
+//            if (i % 10 == 2) count++;
+//            i /= 10;
+//
+//        }
+//
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
+
+
+
+#include<set>
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums1 int整型vector
+     * @param nums2 int整型vector
+     * @return int整型vector
+     */
+ /*   vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> set1;
+        set<int> set2;
+        int* i = set1.begin();
+        int* j = set2.begin();
+        if (set1.size() && set2.sieze()) {
+
+        }
+    }
+};*/
+
+
+
+//#include <iostream>
+//using namespace std;
+//string s;
+//int main() {
+//    cin >> s;
+//    for (auto  &e :  s)
+//
+//}
+
+
+
+
+//#include <string>
+//using namespace std;
+//
+//string reduceString(const string& s) {
+//    string stk;
+//    stk.reserve(s.size());  // 预留空间，减少内存开销
+//    for (char c : s) {
+//        if (!stk.empty() && stk.back() == c) {
+//            // 遇到一对相邻相同字符，弹出即可“删除”
+//            stk.pop_back();
+//        }
+//        else {
+//            // 否则入栈
+//            stk.push_back(c);
+//        }
+//    }
+//    return stk;  // 栈中剩余即为最终形态
+//}
+    
+
+
+    
