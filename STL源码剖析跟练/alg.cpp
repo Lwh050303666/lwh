80b6a465554ff9ff3906bfbf351cee7cee724f5e
lwh <lwh2212789853@gmail.com>
2025-07-14 23:32:22 +0800

    练习

--- STL源码剖析跟练/alg.cpp
+++ STL源码剖析跟练/alg.cpp
@@ -25,18 +25,95 @@ using namespace std;
 // #include<iostream>
 // using namespace std;
 // int fcmp(const void * elem1,const void * elem2)
-template<class T>
-class init{
-public:init():size(3){} 
-~init(){}
-void operator()(){
-    size++;
+// template<class T>
+// class init{
+// public:init():size(3){} 
+// ~init(){}
+// void operator()(){
+//     size++;
 
+// }
+// private: int size=3;
+// };
+// int main(){
+//    init<int> e;
+//    e();
+// }
+
+// template<class T>
+// class ac{
+//     public: T * init() const
+// }
+// template<class T>
+// class auto_ptr{
+// public: explicit auto_ptr():
+
+
+
+// private: T * pointee;
+// };
+ 
+//链表 迭代器
+// template<typename T>
+// class List{
+//     void insert_front(T value);
+//     void insert_end(T value);
+//     void display(ostream &os=cout) const
+// private: ListItem<T>* _end;
+// ListItem<T> * _front;
+// long _size;
+// };
+// template<typename T>
+// class ListItem{
+//     public: T value() const {return _value;}
+//     ListItem * next() const {return _next};
+
+//     private: T _value;
+//     ListItem * _next; //单向链表
+// };
+// //浅拷贝和深拷贝
+// template<class Item>
+// struct ListIter{
+//     Item * ptr;//保持与容器之间的一个联系
+//     ListIter(Item * p=0)//default ctor
+//     :ptr(p){}
+//     //不必实现copy ctor 因为编译器提供的缺省行为已经足够  这里哪怕是默认提供的浅拷贝也够用了   
+//    //因为ptr只是表面自己指向了哪一个链表节点  并没有进行解引用等操作
+//    //不必实现 operator= 因为编译器的缺省行为已经足够
+//    Item & operator *()const {return * ptr;}
+//    Item & operator->()const{return ptr;}
+//    //以下两个operator++遵循标准做法
+//    ListIter & operator++(){ptr=ptr->next(); return *this};
+//    ListIter operator++(int){ListIter tmp=*this; ++*this; return tmp;}
+//    List
+    
+// };
+// //涉及到了资源的管理权问题
+// template<typename T>
+// class List{
+// void insert_end(T value);
+// void insert_front(T value);
+// void display(ostream & os=cout)const;
+// private:  ListItem<T>* _end;
+// ListItem<T>* _front;
+// long _size;
+// };
+// template<typename T>
+// class ListItem{
+// public: T value()const{return _value;}
+// ListItem * next()const{return _next;}
+// private: T _value;
+// ListItem * _next;//单向链表
+// };
+// iterator_traits<int>::value_type;
+void push_back(const T& x){
+    if(finish!=end_of_storage){
+        construct(finish,x);
+        ++finish;
+    }
+    else insert_aux(end(),x);//vector
 }
-private: int size=3;
-};
-int main(){
-   init<int> e;
-   e();
-}
+ (bf'(the fool on the hill));
+ 
+
 
---
+++ cpp_review/learn.cpp
@@ -0,0 +1,239 @@
+// // #include <iostream>
+// // #include <memory>  // 需要包含memory头文件
+// // using namespace std;
+
+// // // shared_ptr<int>ptr1(new int(10));//shared_ptr 当计数减到0时自动释放
+// // // unique_ptr<int>ptr2(new int(20));//unique_ptr 当指针的作用域被销毁就自动释放
+    
+
+// // // try{
+
+// // // }
+// // // catch(const runtime_error &e){
+
+// // // }
+// // // class my_exception:public runtime_error{
+// // //     public:  my_exception(const char * msg ):runtime_error(msg){};
+    
+
+// // // }
+// // // //You must doing something,bro.
+// // // //Just do it.To do the right thing
+
+// // // []()->return {}
+// // // int removeDuplicates(int *nums,int numsSize){
+// // //     if(nums==nullptr ||numsSize<=0)  return 0;
+// // //     int slow=0,fast=1;
+// // //     while(fast<numsSize){
+// // //         if(nums[slow]<nums[fast]){
+// // //             nums[slow+1]=nums[fast];
+// // //             fast++,slow++;   continue;
+// // //         }
+// // //         fast++;
+// // //     }
+// // //     return  slow+1;
+// // // }
+// // // int main(){
+// // //     int nums[8]={0,0,1,1,2,2};
+// // //     removeDuplicates(nums,6);
+// // //     return 0;
+// // // }
+
+// // // int removeElement(int * nums,int numsSize,int val){
+// // //     int cnt=0;
+// // //     for(int i=0;i<numsSize;i++){
+// // //         if(nums[i]==val){
+// // //             cnt++;
+// // //             for(int j=i;j<numsSize-1;j++)  //从前往后  往前挪一位
+// // // {
+// // //     nums[j]=nums[j+1];
+// // // }
+// // //         }
+// // //     }int ans=numsSize-cnt;  
+// // //     return ans ;
+// // // }
+// // // int removeElement(int *nums,int numsSize,int val){
+// // //     int slow=0;
+// // //     for(int fast=0;fast<numsSize;fast++){
+// // //         if(nums[fast]!=val){
+// // //             nums[slow]=nums[fast];
+// // //             slow++;
+// // //         }
+// // //     }
+// // //     return slow;
+// // //
+// // // void merge(int *nums1,int nums1Size,int m,int * nums2,int nums2Size,int n){
+// // //     int end1=m-1,end2=n-1;
+// // //     int dst=m+n-1;
+// // //     while(end1>=0&&end2>=0){
+// // //         if(nums1[end1]>nums2[end2]){
+// // //             nums1[dst--]=nums1[end1--];
+// // //         }
+// // //         else {
+// // //             nums1[dst--]=nums2[end2--];
+// // //         }
+
+// // //     }
+// // //     while(end2>=0)  nums1[dst--]=nums2[end2--];
+
+// // // }
+// // #pragma once
+// // #include<stdio.h>
+// // #include<stdlib.h>
+// // #include<assert.h>
+
+// // typedef int SLDataType;
+// // #define N 100000
+// // //
+// // //静态顺序表 -- 开少了不够用  开多了浪费
+// // //struct SeqList{
+// //     SLDataType a[N];
+// //   int size;
+// // };
+// // typedef int SLDataType;
+// // #define INIT_CAPACITY 4
+
+// // typedef struct SeqList{
+// // SLDataType *a;
+// // int size;
+// // int capacity;
+// // }SL;
+// // //增删查改
+// // void SLInit(SL * ps);
+// // void SLDestroy(SL * ps);
+// // void SLCheckCapacity(SL* ps);
+
+// // void SLPushBack(SL* ps, SLDataType x);
+// // void SLPopBack(SL* ps);
+// // void SLPushFront(SL* ps, SLDataType x);
+// // void SLPopFront(SL* ps);
+// // void SLInsert(SL* ps, int pos, SLDataType x);
+// // void SLErase(SL* ps, int pos);
+// // int SLFind(SL* ps, SLDataType x);
+
+
+// // #include<SeqList.h>
+// // void SLInit(SL * ps){
+// //   assert(ps);
+// //  ps->a= (SLDataType *)malloc(sizeof(SLDataType)*INIT_CAPACITY);
+// // if(ps->a==nullptr){
+// //   perror("malloc fail");
+// //   return;
+// // }
+// // ps->size=0;
+// // ps->capacity=INIT_CAPACITY;
+// // }
+
+// // void SLDestroy(SL* ps){
+// //   assert(ps);
+// //   ps->a=nullptr;
+// //   ps->size=ps->capacity=0;
+// // }
+
+// // void SLPrint(SL * ps){
+// //   assert(ps);
+// //   for(int i=0;i<ps->size;++i){
+// //     printf("%d",ps->a[i]);
+// //   }
+// //   printf("\n");
+// // }
+
+// // void SLCheckCapacity(SL *ps){
+// //   assert(ps);
+// //   if(ps->size==ps->capacity){
+// //     SLDataType * tmp=(SLDataType *)realloc(ps,sizeof(SLDataType)*2*capacity)
+// //     if(tmp==nullptr){
+// //       perror("malloc fail");
+// //       return;
+// //     }
+// //     ps->a=tmp;
+// //     ps->capacity=2*capacity;
+// //   }
+
+// // }
+
+// // void SLPushBack(SL * ps,SLDataType x){
+// //   assert(ps);
+// //   if(ps->size==ps->capacity)  
+// //   SLCheckCapacity(ps);
+// //   if(ps->size==ps->capacity)  return;
+// //   ps->a[size]=x;
+// //   SLInsert(ps,ps->size,x);
+// // }
+
+// // void SLPopBack(SL * ps){
+// //   assert(ps);
+// //   if(ps->size==0)  return;
+// //   ps->size--;
+// // }
+
+// // void SLPushFront(SL *ps,SLDataType x){
+// //   assert(ps);  //从后往前向后挪一位
+// //   SLCheckCapacity(ps);
+// // if(ps->size==0) {
+// //   ps->a[0]=x;  ps->size++;
+// // }
+// // else {
+// //   int end=ps->size-1;
+// //    while(end>=0){
+// //     ps->a[end+1]=ps->a[end];
+// //    }
+// //    ps->a[0]=x;
+// // }
+// // }
+
+// // void SLInsert(SL * ps,int pos,SLDataType x){
+// // assert(ps);
+// // assert(pos>=0&&pos<=ps->size-1);
+// // SLCheckCapacity(ps);    //从后往前向后挪一位
+// // int end=ps->size-1;
+// // while(end>=pos){
+// //   ps->a[end+1]==ps->a[end];
+// // }
+// // ps->a[pos]=x;
+// // ps->size++;
+// // }
+
+// // void SLErase(SL *ps,int pos){//从前往后依次往前挪一位
+// //       assert(ps);
+      
+// // }
+
+// // int SLFind(SL * ps,SLDataType x){
+// // assert(ps);
+// // for(int i=0,i<ps->size;i++){
+// //   if(ps->a[i]==x)  return i;
+// // }
+// // return -1;
+
+// // }
+
+// //单链表
+// #pragma once
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<assert.h>
+// typedef int SLTDataType;
+// typedef struct SListNode{
+//   SLTDataType data;
+//   struct SlistNode * next;
+
+// }SLTNode;
+
+// void SLTprint(SLTNode *phead);
+
+// void SLTPushBack(SLTNode ** pphead)
+
+// SLTNode * Find(SLTNode *phead,SLTDataType x){
+// while(phead->next!=nullptr){
+//   if(phead->data!=x)  phead=phead->next;
+//   else  return  phead;
+// }
+// return nullptr;
+// }
+ListNode * mergeTwoLists(struct ListNode * list1,struct ListNode  )
+
+
+
+
+
