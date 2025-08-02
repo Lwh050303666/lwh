cc7cb084f48579712530797bf9ece8cf27492cef
lwh <lwh2212789853@gmail.com>
2025-08-02 23:51:07 +0800

    练习

--- lesson40/sem_cp/Task.hpp
+++ lesson40/sem_cp/Task.hpp
@@ -1,86 +1,80 @@
 #pragma once
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+using namespace std;
 
-std::string opers="+-*/%";
+string opers="+-*/%";
 
 enum{
     DivZero=1,
     ModZero,
-    Unknown
+    UnKnown
+
 };
 
-class Task
-{
-public:
-    Task()
-    {}
-    Task(int x, int y, char op) : data1_(x), data2_(y), oper_(op), result_(0), exitcode_(0)
-    {
-    }
-    void run()
-    {
-        switch (oper_)
-        {
-        case '+':
-            result_ = data1_ + data2_;
-            break;
-        case '-':
-            result_ = data1_ - data2_;
-            break;
-        case '*':
-            result_ = data1_ * data2_;
-            break;
-        case '/':
-            {
-                if(data2_ == 0) exitcode_ = DivZero;
-                else result_ = data1_ / data2_;
-            }
-            break;
-        case '%':
-           {
-                if(data2_ == 0) exitcode_ = ModZero;
-                else result_ = data1_ % data2_;
-            }            break;
-        default:
-            exitcode_ = Unknown;
-            break;
-        }
-    }
-    void operator ()()
-    {
-        run();
-    }
-    std::string GetResult()
-    {
-        std::string r = std::to_string(data1_);
-        r += oper_;
-        r += std::to_string(data2_);
-        r += "=";
-        r += std::to_string(result_);
-        r += "[code: ";
-        r += std::to_string(exitcode_);
-        r += "]";
+class Task{
+public: Task(){
 
-        return r;
-    }
-    std::string GetTask()
-    {
-        std::string r = std::to_string(data1_);
-        r += oper_;
-        r += std::to_string(data2_);
-        r += "=?";
-        return r;
-    }
-    ~Task()
-    {
-    }
+}
+Task(int x,int y,char op):data1_(x),data2_(y),oper_(op),result_(0),exitcode_(0)
+{
+
+}
+
+void run()
+{
+    switch(oper_){
+case '+':
+result_=data1_+data2_;  break;
+case '-':
+result_=data1_-data2_;  break;
+case '*':
+result_=data1_*data2_;  break;
+case '/':
+    if(data2_==0)  exitcode_=DivZero;
+    else result_=data1_/data2_;  break;
+    case '%':
+    if(data2_==0)  exitcode_=ModZero;
+    else result_=data1_%data2_;  break;
+    
+    default:
+       exitcode_=UnKnown;
+       break;
+}}
+
+void operator()(){
+    run();
+}
+ 
+ string GetResult(){
+    string r=to_string(data1_);
+    r+=oper_;
+    r+=to_string(data2_);
+    r+= '=';
+    r+=to_string(result_);
+    r+= "[Code:";
+    r+=to_string(exitcode_);
+    r+= "]";
+    return r;
+ }
+ string GetTask(){
+    string r=to_string(data1_);
+    r+=oper_;
+    r+=to_string(data2_);
+    r+="=?";
+    return r;
+ }
+ ~Task()
+ {
+    
+ }
+
 
 private:
-    int data1_;
-    int data2_;
-    char oper_;
+int data1_;
+int data2_;
+char oper_;
+int result_;
+int exitcode_;
 
-    int result_;
-    int exitcode_;
 };
